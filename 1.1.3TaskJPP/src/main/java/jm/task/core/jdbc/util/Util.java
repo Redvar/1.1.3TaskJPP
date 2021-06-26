27d075cbeab521ded8e3e657cdb66cbf6a7be613
Pavel Konoplin <43984568+Redvar@users.noreply.github.com>
2021-06-27 01:43:22 +0300

    Update Util.java

--- 1.1.3TaskJPP/src/main/java/jm/task/core/jdbc/util/Util.java
+++ 1.1.3TaskJPP/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -13,7 +13,7 @@ public class Util {
         private static Connection connection;
         private static final String URL = "jdbc:mysql://localhost:3306/mydb";
         private static final String USER = "root";
-        private static final String PASS = "glhf1941WAW";
+        private static final String PASS = "root";
 
         public static Connection getConnection() {
             try {
